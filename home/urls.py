50843938d0813ae4d4c7de7a9b52ecba7c27fa34
akshay-joseph-justin <akshayjosephjustin721@gmail.com>
2024-03-19 19:16:45 +0530

    user authentication

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="home"),
     path('product/', views.product),
     path('cart/', views.cart),
-    path('login/pppp', views.login),
 ]
