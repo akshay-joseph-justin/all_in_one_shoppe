078c8eff22e2038d6ee8d3ead2c78b342458afcc
akshay-joseph-justin <akshayjosephjustin721@gmail.com>
2024-03-19 21:43:00 +0530

    backend

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="home"),
     path('product/', views.product),
-    path('cart/', views.cart),
+    path('cart/', views.cart, name="cart"),
 ]
