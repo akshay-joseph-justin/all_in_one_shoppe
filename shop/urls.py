5de911fc552f04ae9beeba313c082c7c055b394b
Sreekanth S <sreekanthsanthosh17@gmail.com>
2024-03-20 16:16:07 +0530

    minor update

--- shop/urls.py
+++ shop/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('home.urls', 'home'), namespace="home")),
-    path('account', include(('users.urls', 'users'), namespace="users")),
+    path('account/', include(('users.urls', 'users'), namespace="users")),
 ]
