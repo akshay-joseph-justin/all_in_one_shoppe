8cce6a94bcc0b6b63bdc194b141260b15f4a0f53
adeebatgithub <adeebdanish063@gmail.com>
2024-03-27 13:50:20 +0530

    test-2

--- users/decorators.py
+++ users/decorators.py
@@ -2,13 +2,13 @@ from django.shortcuts import redirect
 from functools import wraps
 
 
-def redirect_authenticated_user(view, redirect_to='home:shop'):
+def redirect_authenticated_user(view, redirect_to='users:profile-update'):
     @wraps(view)
     def wrapper(request, *args, **kwargs):
         if request.user.is_authenticated:
             if request.user.is_staff:
                 return redirect('mod:home')
-            return redirect(redirect_to)
+            return redirect(redirect_to, name=request.user.username)
         else:
             return view(request, *args, **kwargs)
     return wrapper
