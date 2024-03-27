01373c771fc6f9f48f5c11e45a04fd1ecd66f936
Sreekanth S <sreekanthsanthosh17@gmail.com>
2024-03-27 21:30:24 +0530

    temp

--- home/filters.py
+++ home/filters.py
@@ -1,11 +1,11 @@
 import django_filters
 from . import models
-
+from django import forms
 
 class ProductFilter(django_filters.FilterSet):
-    size = django_filters.CharFilter(lookup_expr="icontains")
-    colour = django_filters.CharFilter(lookup_expr="icontains")
-
+    size = django_filters.CharFilter(lookup_expr="icontains", widget=forms.TextInput(attrs={"placeholder": "M, L, XL", "class": "form-control"}))
+    colour = django_filters.CharFilter(lookup_expr="icontains", widget=forms.TextInput(attrs={"placeholder": "choose your color", "class": "form-control"}))
+    category = django_filters.ModelChoiceFilter(queryset=models.CategoryModel.objects.all(), widget=forms.Select(attrs={"class": "form-select"}))
     class Meta:
         model = models.ProductModel
         fields = ("category", "size", "colour")
